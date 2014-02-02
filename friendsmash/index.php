6ac94256e7d93f7eb5ed587e38962bb341aa5fc2
Nguyen Ngoc Hoang <nguyenngochoang89@gmail.com>
2014-02-02 19:11:04 +0900

    change

--- friendsmash/index.php
+++ friendsmash/index.php
@@ -1,63 +1,103 @@
 <?php
 
 /**
-* Copyright 2013 Facebook, Inc.
-*
-* You are hereby granted a non-exclusive, worldwide, royalty-free license to
-* use, copy, modify, and distribute this software in source code or binary
-* form for use in connection with the web services and APIs provided by
-* Facebook.
-*
-* As with any software that integrates with the Facebook platform, your use
-* of this software is subject to the Facebook Developer Principles and
-* Policies [http://developers.facebook.com/policy/]. This copyright notice
-* shall be included in all copies or substantial portions of the software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-* DEALINGS IN THE SOFTWARE.
-*/
+ * Copyright 2013 Facebook, Inc.
+ *
+ * You are hereby granted a non-exclusive, worldwide, royalty-free license to
+ * use, copy, modify, and distribute this software in source code or binary
+ * form for use in connection with the web services and APIs provided by
+ * Facebook.
+ *
+ * As with any software that integrates with the Facebook platform, your use
+ * of this software is subject to the Facebook Developer Principles and
+ * Policies [http://developers.facebook.com/policy/]. This copyright notice
+ * shall be included in all copies or substantial portions of the software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
 ?>
+<?php
+require 'server/fb-php-sdk/facebook.php';
 
+$app_id = '477729098999613';
+$app_secret = 'a094d353913fed48e199c18fc6711023';
+$app_namespace = 'friendsmashhoangnn';
+$app_url = 'https://apps.facebook.com/' . $app_namespace . '/';
+$scope = 'email,publish_actions';
+
+// Init the Facebook SDK
+$facebook = new Facebook(array(
+    'appId' => $app_id,
+    'secret' => $app_secret,
+));
+
+// Get the current user
+$user = $facebook->getUser();
+
+// If the user has not installed the app, redirect them to the Login Dialog
+if (!$user) {
+    $loginUrl = $facebook->getLoginUrl(array(
+        'scope' => $scope,
+        'redirect_uri' => $app_url,
+    ));
+
+    print('<script> top.location.href=\'' . $loginUrl . '\'</script>');
+}
+?>
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
     <title>Friend Smash!</title>
 
-      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-      <meta name="apple-mobile-web-app-capable" content="yes" />
-      <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-      <meta property="og:image" content="http://www.friendsmash.com/images/logo_large.jpg"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+    <meta name="apple-mobile-web-app-capable" content="yes"/>
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
+    <meta property="og:image" content="http://www.friendsmash.com/images/logo_large.jpg"/>
 
-      <link href="scripts/style.css" rel="stylesheet" type="text/css">
+    <link href="scripts/style.css" rel="stylesheet" type="text/css">
 
-      <script src="scripts/jquery-1.8.3.js"></script>
-      <script src="scripts/jquery.jCounter-0.1.4.js"></script>
+    <script src="scripts/jquery-1.8.3.js"></script>
+    <script src="scripts/jquery.jCounter-0.1.4.js"></script>
 
-  </head>
+</head>
 
-  <body>
-      <div id="fb-root"></div>
-      <script src="//connect.facebook.net/en_US/all.js"></script>
+<body>
 
-      <div id="topbar">
-        <img src="images/logo.jpg"/>
-      </div>
+<div id="fb-root"></div>
+<script src="//connect.facebook.net/en_US/all.js"></script>
 
-      <div id="stage">
-        <div id="gameboard">
-            <canvas id="myCanvas"></canvas>
-        </div>
-      </div>
+<div id="topbar">
+    <img src="images/logo.jpg"/>
+</div>
 
-      <script src="scripts/core.js"></script>
-      <script src="scripts/game.js"></script>
-      <script src="scripts/ui.js"></script>
-      
-  </body>
+<div id="stage">
+    <div id="gameboard">
+        <canvas id="myCanvas"></canvas>
+    </div>
+</div>
+
+<script src="scripts/core.js"></script>
+<script src="scripts/game.js"></script>
+<script src="scripts/ui.js"></script>
+<script>
+    var appId = '<?php echo $facebook->getAppID() ?>';
+    // Initialize the JS SDK
+    FB.init({
+        appId: appId,
+        cookie: true,
+    });
+
+    FB.getLoginStatus(function (response)
+    {
+        uid = response.authResponse.userID ? response.authResponse.userID : null;
+    });
+</script>
+</body>
 </html>
